981da53625a83fb7f2fc25ba3b945f1b441ea04f
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-04-11 08:13:00 +0100

    function expression & global (dir & file names)

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-var time = 0;
-var timer = setInterval(function(){
-    time += 2;
-    console.log(time + " seconds have passed");
+//Get directory name
+console.log('Your are in ' + __dirname + ' directory');
 
-    if(time >= 10){
-        clearInterval(timer);
-    }
-},2000);
+//Get file name
+console.log('Your file name is ' + __filename);
+
+//Function Expressions
+
+//Normal js function
+function sayHi(){
+    console.log("I said Hi");
+}
+
+sayHi(); //calls the function
+
+//func expression in node
+
+var sayBye = function(){
+    /*here, an anonymous function is
+    assigned to a variable
+    making the variable a function*/
+    console.log("Bye Bye");
+}
+sayBye(); //calls the function
+
+//calling a function within a function
+function callFunction(fun){
+    fun();
+}
+
+callFunction(sayBye); //calls the sayBye function
