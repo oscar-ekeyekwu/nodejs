34f0e0c1540fafbd7738665571fb1d3c4de0fac6
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-04-11 17:04:47 +0100

    node event emiiter & utils.inherit

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 //Modules and Export
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 /**
  * here you assign the exported module to a
  * variable, so you can use it where it is
  * required
  */
 
-console.log(stuff.counter(['Oscar', 'Ikechukwu']));
-console.log(stuff.adder(5, 8));
-console.log(stuff.pi);
+var myEmitter = new events.EventEmitter();
+var Person = function (name) {
+  this.name = name;
+};
+
+//makig the person constructor to inherit the events.Emitter
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+
+var people = [james, mary, ryu];
+
+//loop throught the people array created and assign the evennts
+people.forEach(function (person) {
+  person.on('speak', function (msg) {
+    console.log(person.name + ' said: ' + msg);
+  });
+});
+//add the event emitter to the instance of the emitter constructor
+myEmitter.on('someEvent', function (msg) {
+  console.log(msg);
+});
+
+//emit the event more like a trigger for the event emitter
+myEmitter.emit('someEvent', 'the event was fired');
+james.emit('speak', 'hello people');
+ryu.emit('speak', 'good day neighbours');
